9ef6c92bf7d1aed0cf91fb06375837f7bc928c8e
Dan Boehmke <Daniel@webappclouds.com>
2019-10-23 16:39:02 -0400

    Activity lifecycle

--- app/src/main/java/com/webappclouds/noteapp/MainActivity.kt
+++ app/src/main/java/com/webappclouds/noteapp/MainActivity.kt
@@ -41,6 +41,31 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show()
     }
 
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "onPause", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "onStop", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this, "onRestart", Toast.LENGTH_LONG).show()
+    }
+
     fun LoadQuery(title: String) {
         var dbManager = DbManager(this)
         val projections = arrayOf("ID", "Title", "Description")
