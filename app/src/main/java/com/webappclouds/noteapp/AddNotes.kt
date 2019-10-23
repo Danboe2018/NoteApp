ca9b97a9b20667d9b8a36c29b95ee7e277351f7b
Dan Boehmke <Daniel@webappclouds.com>
2019-10-23 12:54:37 -0400

    Menu Bar

--- app/src/main/java/com/webappclouds/noteapp/AddNotes.kt
+++ app/src/main/java/com/webappclouds/noteapp/AddNotes.kt
@@ -1,12 +1,19 @@
 package com.webappclouds.noteapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class AddNotes : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_notes)
+    }
+
+    fun butAdd(view:View){
+        var intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
     }
 }
